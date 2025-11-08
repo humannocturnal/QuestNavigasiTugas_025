4d881598dd4c839782055f46bb683ed4e2dc1acb
humannocturnal <syaputrar374@gmail.com>
2025-11-08 18:15:49 +0700

    buat composable Home dengan background gradient

--- app/src/main/java/com/example/questnavigasitugas_025/Home.kt
+++ app/src/main/java/com/example/questnavigasitugas_025/Home.kt
@@ -1,8 +1,33 @@
 package com.example.questnavigasitugas_025
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 private val NeonBlue   = Color(0xFF4F46E5)
 private val NeonPurple = Color(0xFF7C3AED)
 private val DarkBgTop  = Color(0xFF020617)
 private val DarkBgBot  = Color(0xFF020617)
+
+@Composable
+fun Home(navController: NavController){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(DarkBgTop,DarkBgBot)
+                )
+            )
+            .padding(20.dp)
+    ){
+
+    }
+}
