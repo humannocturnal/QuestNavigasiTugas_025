54ab41e5ba9a7ec049ca2ddfe1efa11bff77531d
humannocturnal <syaputrar374@gmail.com>
2025-11-08 18:19:26 +0700

    tambahkan teks sambutan "Selamat Datang" di Home

--- app/src/main/java/com/example/questnavigasitugas_025/Home.kt
+++ app/src/main/java/com/example/questnavigasitugas_025/Home.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -37,7 +40,13 @@ fun Home(navController: NavController){
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-
+            Text(
+                text = "Selamat Datang",
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    color = Color.White,
+                    fontWeight = FontWeight.SemiBold
+                )
+            )
         }
     }
 }
