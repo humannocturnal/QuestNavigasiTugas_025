92fa90d5a3cc0d275169b6cd03527d60b9c9da89
humannocturnal <syaputrar374@gmail.com>
2025-11-08 19:31:35 +0700

    buat halaman Formulir dengan input dan opsi pendaftaran peserta

--- app/src/main/java/com/example/questnavigasitugas_025/view/IsiFormulir.kt
+++ app/src/main/java/com/example/questnavigasitugas_025/view/IsiFormulir.kt
@@ -1,4 +1,43 @@
 package com.example.questnavigasitugas_025.view
 
-class IsiFormulir {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+
+
+private val NeonBlue   = Color(0xFF4F46E5)
+private val NeonPurple = Color(0xFF7C3AED)
+
+@Composable
+fun Formulir(navController: NavController) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+
+    val JK_Option = listOf("Laki-laki", "Perempuan")
+    var selectedJK by remember { mutableStateOf(JK_Option[0]) }
+
+    val SP_Option = listOf("Menikah", "Belum Menikah", "Duda/janda")
+    var selectedSP by remember { mutableStateOf(SP_Option[0]) }
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    listOf(Color(0xFF020617), Color(0xFF020617))
+                )
+            )
+    ) {
+    }
 }
