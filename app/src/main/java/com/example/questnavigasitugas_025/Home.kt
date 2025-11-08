c946cce25b4d7d218cd74699e6bc2d6a18c0f834
humannocturnal <syaputrar374@gmail.com>
2025-11-08 18:32:42 +0700

    pasang logo di tengah efek cahaya NeonPurple

--- app/src/main/java/com/example/questnavigasitugas_025/Home.kt
+++ app/src/main/java/com/example/questnavigasitugas_025/Home.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigasitugas_025
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -20,6 +21,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -88,8 +91,12 @@ fun Home(navController: NavController){
                             ),
                         contentAlignment = Alignment.Center
                     ) {
-
-                        
+                        Image(
+                            painter = painterResource(id = R.drawable.logo),
+                            contentDescription = null,
+                            modifier = Modifier.size(80.dp),
+                            contentScale = ContentScale.Crop
+                        )
                     }
                 }
             }
