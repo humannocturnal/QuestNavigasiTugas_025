923225a321e9642bf181c16ea579b0f57381fd2d
humannocturnal <syaputrar374@gmail.com>
2025-11-08 18:29:40 +0700

    tambahkan Box efek neon di dalam card halaman Home

--- app/src/main/java/com/example/questnavigasitugas_025/Home.kt
+++ app/src/main/java/com/example/questnavigasitugas_025/Home.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -71,7 +72,26 @@ fun Home(navController: NavController){
                 ),
                 elevation = CardDefaults.cardElevation(12.dp)
             ) {
+                Column(
+                    modifier = Modifier.padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
+                    Box(
+                        modifier = Modifier
+                            .size(120.dp)
+                            .background(
+                                brush = Brush.radialGradient(
+                                    listOf(NeonPurple, Color.Transparent)
+                                ),
+                                shape = RoundedCornerShape(32.dp)
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+                        
+                    }
+                }
             }
 
         }
