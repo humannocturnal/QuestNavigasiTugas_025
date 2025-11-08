ad8c8ca21f869f8c6b546763aef304a2c085bbb4
humannocturnal <syaputrar374@gmail.com>
2025-11-08 20:06:14 +0700

    hubungkan Home, Formulir, dan TampilData dalam navigasi utama Compose

--- app/src/main/java/com/example/questnavigasitugas_025/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasitugas_025/MainActivity.kt
@@ -12,36 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questnavigasitugas_025.ui.theme.QuestNavigasiTugas_025Theme
+import com.example.questnavigasitugas_025.view.MainApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            QuestNavigasiTugas_025Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestNavigasiTugas_025Theme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/questnavigasitugas_025/view/MainApp.kt
+++ app/src/main/java/com/example/questnavigasitugas_025/view/MainApp.kt
@@ -1,2 +1,18 @@
 package com.example.questnavigasitugas_025.view
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasitugas_025.Home
+
+@Composable
+fun MainApp() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "Home") {
+        composable("Home") { Home(navController) }
+        composable("Tampil_Data") { TampilData(navController) }
+        composable("Formulir") { Formulir(navController) }
+    }
+}
