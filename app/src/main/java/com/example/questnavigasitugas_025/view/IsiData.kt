6f23a1009bbd279217e25ef38d36f8e05cdb05f3
humannocturnal <syaputrar374@gmail.com>
2025-11-08 18:52:04 +0700

    buat data class Peserta dan tambah palet warna NeonBlue & NeonPurple

--- app/src/main/java/com/example/questnavigasitugas_025/view/IsiData.kt
+++ app/src/main/java/com/example/questnavigasitugas_025/view/IsiData.kt
@@ -1,2 +1,13 @@
 package com.example.questnavigasitugas_025.view
 
+import androidx.compose.ui.graphics.Color
+
+private val NeonBlue   = Color(0xFF4F46E5)
+private val NeonPurple = Color(0xFF7C3AED)
+
+data class Peserta(
+    val nama: String,
+    val jk: String,
+    val status: String,
+    val alamat: String
+)
